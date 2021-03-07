e3c5e21e10fc2347bf44ce6da830b549d18daf6a
homecyclist <49373727+homecyclist@users.noreply.github.com>
2021-03-06 21:56:02 -0500

    Cleanup indents.

--- main.c
+++ main.c
@@ -17,15 +17,15 @@ void timer0_pwm(void)
 	TCCR0A	= (1<<WGM00);	//Fast PWM, Phase Correct.
 	TCCR0A |= (1<<COM0B1);	//Toggle OC0B(PB1) on BOTTOM
 	TCCR0B	= (1<<CS00);	//F_CPU / 1
-	OCR0B	= 64;			//Duty Cycle, 0-255
+	OCR0B	= 64;		//Duty Cycle, 0-255
 }
 
 int main(void)
 {
 	DDRB |= (1<<DDB1);
 	timer0_pwm();
-    while(1)
-    {
+	while(1)
+	{
 		// Something to set OCR0B (duty cycle) in response to something. 
-    }
+    	}
 }
